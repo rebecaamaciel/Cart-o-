b693d774cb383258f394463acea625e42ac50446
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-09-23 11:43:00 -0300

    jjj

--- main.js
+++ main.js
@@ -3,4 +3,8 @@
 function soma(numero1,numero2){
     return numero1+numero2;
 }
-console log(soma(1,2));
+console.log(soma(1,2));
+function areaQuadrado(lado){
+    return lado**2;
+}
+console.log(areaQuadrado(6));
