04c3d7e32461cf141654f1ee7e912cc3737bdcd4
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-09-30 11:22:17 -0300

    ttt

--- main.js
+++ main.js
@@ -24,3 +24,4 @@
 //     return(num1+num2+num3)/3;
 // }
 // document.write(media(MedirMedia1,MedirMedia2,MedirMedia3));
+    let estados =['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
